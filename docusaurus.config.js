2a6963723b7d49bbdf12894fd52e5ea74a8ad2bf
HeZean <realhezean@gmail.com>
2022-08-23 23:12:31 +0800

    feat

---
+++ docusaurus.config.js
@@ -0,0 +1,142 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Feature Probe',
+  tagline: 'An open source feature management service',
+  url: 'https://YZL24.github.io',
+  baseUrl: '/docs',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  deploymentBranch: 'gh-pages',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'FeatureProbe', // Usually your GitHub org/user name.
+  projectName: 'feature-probe-docs', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'zh-CN'],
+    localeConfigs: {
+      en: {
+        label: 'English',
+      },
+      'zh-CN': {
+        label: '中文（中国）',
+      },
+    },
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/', 
+          editUrl: 'https://github.com/FeatureProbe/feature-probe-docs/tree/main/',
+          editLocalizedFiles: true,
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl: 'https://github.com/FeatureProbe/feature-probe-docs/tree/main/',
+          editLocalizedFiles: true,
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: '',
+        logo: {
+          alt: 'FeatureProbe Logo',
+          src: 'img/logo.svg',
+          srcDark: 'img/logo-light.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'introduction/what',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            to: '/blog', 
+            label: 'Blog', 
+            position: 'left'
+          },
+          {
+            type: 'localeDropdown',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/FeatureProbe',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Product',
+            items: [
+              {
+                label: 'Docs',
+                to: '/',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Slack',
+                href: 'https://join.slack.com/t/featureprobe/shared_invite/zt-1b5qd120x-R~dDbpgL85GgCLTtfNDj0Q',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/featureprobe',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/FeatureProbe/FeatureProbe',
+              },
+            ],
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ['java'],
+      },
+    }),
+};
+
+module.exports = config;
---
+++ i18n/zh-CN/docusaurus-plugin-content-docs/current/sdk/server-sdk-rust.md
@@ -0,0 +1,109 @@
+---
+sidebar_position: 7
+---
+
+# 服务端 SDK Rust
+
+## 快速尝试 Demo Code
+
+我们提供了一个可运行的演示代码，让您了解如何使用 FeatureProbe SDK。
+
+1. 使用 docker composer 启动 FeatureProbe 服务。 [How to](https://github.com/FeatureProbe/FeatureProbe#1-starting-featureprobe-service-with-docker-compose)
+
+2. 下载此 repo 并运行演示程序：
+
+ ```bash
+ git clone https://github.com/FeatureProbe/server-sdk-rust.git
+ cd server-sdk-rust
+ cargo run --example demo
+ ```
+
+3. 从此处下载 [examples](https://github.com/FeatureProbe/server-sdk-rust/tree/main/examples), 做一些修改并再次运行程序
+
+ ```bash
+ cargo run --example demo
+ ```
+
+## 分步指南
+
+在本指南中，我们解释了如何使用 FeatureProbe 在 Rust 应用程序中使用功能开关。
+
+### 步骤 1. 安装 FeatureProbe SDK
+
+首先，在您的应用程序中安装 FeatureProbe SDK 作为依赖项：
+
+```shell
+cargo install cargo-edit
+cargo add feature-probe-server-sdk-rs --allow-prerelease
+```
+
+然后，在代码中引入：
+
+```rust
+use feature_probe_server_sdk::{FPConfig, FPUser, FeatureProbe};
+```
+
+### 步骤 2. 创建一个 FeatureProbe instance
+
+安装并导入 SDK 后，创建 FeatureProbe sdk 的单个共享实例。
+
+```rust
+fn main() {
+    let remote_url = "http://localhost:4007";
+
+    let config = FPConfig {
+        remote_url: remote_url.to_owned(),
+        server_sdk_key: args.server_sdk_key.to_owned(),
+        refresh_interval: Duration::from_secs(5),
+        wait_first_resp: true,
+    };
+
+    let fp = match FeatureProbe::new(config) {
+        Ok(fp) => fp,
+        Err(e) => {
+            tracing::error!("{:?}", e);
+            return;
+        }
+    };
+}
+```
+
+### 步骤 3. 使用 FeatureProbe 开关获取设置的值
+
+您可以使用 sdk 拿到对应开关名设置的值。
+
+```rust
+let user = FPUser::new("user@company.com").with("name", "bob");
+let show_feature = fp.bool_value("your.toggle.key", &user, false);
+
+if show_feature {
+    # application code to show the feature
+} else {
+    # the code to run if the feature is off
+}
+```
+
+### 步骤 4. 单元测试 (可选)
+
+```rust
+let fp = FeatureProbe::new_for_test("toggle_1", Value::Bool(false));
+let u = FPUser::new("key");
+assert_eq!(fp.bool_value("toggle_1", &u, true), false);
+
+let mut toggles: HashMap<String, Value> = HashMap::new();
+toggles.insert("toggle_2".to_owned(), json!(12.5));
+toggles.insert("toggle_3".to_owned(), json!("value"));
+let fp = FeatureProbe::new_for_tests(toggles);
+assert_eq!(fp.number_value("toggle_2", &u, 20.0), 12.5);
+assert_eq!(fp.string_value("toggle_3", &u, "val".to_owned()), "value");
+```
+
+## 测试
+
+我们对所有 SDK 进行了统一的集成测试。集成测试用例作为每个 SDK 存储库的子模块添加。所以
+在运行测试之前，请务必先拉取子模块以获取最新的集成测试。
+
+```shell
+git pull --recurse-submodules
+cargo test
+```
